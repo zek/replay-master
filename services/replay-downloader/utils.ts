9ccc38d3cac10e1f78cb2a7cde4945d29b4b7795
Talha Zekeriya Durmuş <drtzack@gmail.com>
2024-04-13 05:47:21 -0500

    create match history fetcher

--- services/replay-downloader/utils.ts
+++ services/replay-downloader/utils.ts
@@ -1,7 +1,21 @@
 import type GlobalOffensive from "globaloffensive";
-import type { MatchType, ReplayMeta} from "./types";
+import { request } from "undici";
+import type { MatchType, ReplayMeta } from "./types";
 import { ReplayOrigin } from "./types";
 
+export interface MatchHistoryResponse {
+  result: {
+    nextcode: string;
+  };
+}
+
+export type MatchRequest = {
+  steamId: string,
+  authCode: string,
+  lastShareCode: string,
+  steamApiKey: string,
+  sleep?: number
+}
 
 export function matchToReplayMeta(match: GlobalOffensive.Match, matchToken: string): ReplayMeta {
   const playerCount = match.roundstatsall[0]?.reservation.account_ids.filter((id) => id !== 0)
@@ -26,3 +40,52 @@ export function matchToReplayMeta(match: GlobalOffensive.Match, matchToken: stri
     sharecode: matchToken,
   };
 }
+
+
+export const getNextMatchCode = async ({
+  authCode, lastShareCode, steamId, steamApiKey,
+}: MatchRequest): Promise<string | null> => {
+  const url = new URL("https://api.steampowered.com/ICSGOPlayers_730/GetNextMatchSharingCode/v1");
+  url.searchParams.append("access_token", steamApiKey);
+  url.searchParams.append("steamid", steamId);
+  url.searchParams.append("steamidkey", authCode);
+  url.searchParams.append("knowncode", lastShareCode);
+
+  const response = await request(url, { method: "GET" });
+
+  if (response.statusCode < 200 || response.statusCode > 299) {
+    throw new Error(`Unexpected status code ${response.statusCode}. Response body: ${await response.body.text()}`);
+  }
+
+  const data: MatchHistoryResponse = await response.body.json() as MatchHistoryResponse;
+  if (!data?.result?.nextcode || data.result.nextcode === "n/a") {
+    return null;
+  }
+  return data.result.nextcode;
+};
+
+export async function* matchCodeGenerator(
+  { steamId, steamApiKey, authCode, lastShareCode, sleep }: MatchRequest,
+): AsyncGenerator<string, void> {
+  let nextShareCode: string | null = lastShareCode;
+
+  do {
+    nextShareCode = await getNextMatchCode({
+      steamId,
+      authCode,
+      lastShareCode: nextShareCode,
+      steamApiKey,
+    });
+    if (nextShareCode) {
+      yield nextShareCode;
+    }
+
+    if (sleep) {
+      await new Promise(r => setTimeout(r, sleep));
+    }
+  }
+  while (nextShareCode);
+}
+
+
+
